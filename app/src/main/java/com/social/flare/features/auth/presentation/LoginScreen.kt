3611c49d513a9abac17d596cda21493f998d4867
AndreW7362 <137661916+AndreW7362@users.noreply.github.com>
2026-05-30 21:11:06 -0500

    Actualizar diseño del login

--- app/src/main/java/com/social/flare/features/auth/presentation/LoginScreen.kt
+++ app/src/main/java/com/social/flare/features/auth/presentation/LoginScreen.kt
@@ -1,5 +1,4 @@
 package com.social.flare.features.auth.presentation
-
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
