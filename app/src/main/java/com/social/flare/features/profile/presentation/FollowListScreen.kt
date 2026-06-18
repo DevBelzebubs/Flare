821b91a7a232262855fab19fa28ac023cc20de39
locots <locotsxtreem05@gmail.com>
2026-06-17 20:51:55 -0500

    Fix follow list top bar spacing

--- app/src/main/java/com/social/flare/features/profile/presentation/FollowListScreen.kt
+++ app/src/main/java/com/social/flare/features/profile/presentation/FollowListScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -76,7 +77,8 @@ fun FollowListScreen(
                         Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back", tint = colorScheme.onBackground)
                     }
                 },
-                colors = TopAppBarDefaults.topAppBarColors(containerColor = colorScheme.background)
+                colors = TopAppBarDefaults.topAppBarColors(containerColor = colorScheme.background),
+                windowInsets = WindowInsets(0, 0, 0, 0)
             )
         },
         containerColor = colorScheme.background
